b02af4a661f84b2b59ab1fccb572790e38797834
c-delouvencourt <clement@meilleursbiens.com>
2024-10-16 09:52:49 +0200

    Finish the library and add webhook handler

--- src/AircallServiceProvider.php
+++ src/AircallServiceProvider.php
@@ -3,35 +3,18 @@
 namespace CLDT\Aircall;
 
 use CLDT\Aircall\Http\Controllers\AircallWebhooksController;
-use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\ServiceProvider;
-use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
 class AircallServiceProvider extends PackageServiceProvider
 {
-
-    public function register()
-    {
-        $this->app->singleton('aircall', function (){
-            return new Aircall();
-        });
-    }
-
     public function configurePackage(Package $package): void
     {
         $package
             ->name('aircall')
             ->hasConfigFile()
-            ->hasMigration('create_aircall_webhook_calls_table')
-            ->hasInstallCommand(function(InstallCommand $command) {
-                $command
-                    ->publishConfigFile()
-                    ->askToStarRepoOnGitHub('c-delouvencourt/laravel-aircall');
-            });
-        ;
+            ->hasMigration('create_aircall_webhook_calls_table');
     }
 
     public function bootingPackage()
